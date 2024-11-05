abba00ee86a76920df9f4f408f618310aa769995
Veetinho <comicbookguy.jeffrey@gmail.com>
2024-11-05 23:08:13 +0100

    simple charts added

---
+++ main.js
@@ -0,0 +1,29 @@
+const ctx = document.getElementById('myChart')
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Another'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+      {
+        label: 'My First Dataset',
+        data: [55, 45, 77, 88, 60, 56, 42],
+        fill: false,
+        borderColor: 'green',
+        tension: 0.1,
+      },
+      {
+        type: 'bar',
+        label: 'Bar Dataset',
+        data: [5, 7, 7, 6, 4, 4],
+      },
+    ],
+  },
+})
