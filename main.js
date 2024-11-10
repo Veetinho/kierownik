ab2f8b5f3013ae4e874d614059d185828459a33f
Veetinho <comicbookguy.jeffrey@gmail.com>
2024-11-11 00:58:24 +0100

    added functionality with calendar

--- main.js
+++ main.js
@@ -1,8 +1,94 @@
 const _ = (id) => document.getElementById(id)
+const chartJobPlan = _('chartJobPlan')
+const chartEmployeesPlan = _('chartEmployeesPlan')
+const planDateStart = _('planDateStart')
+const planDateEnd = _('planDateEnd')
 
-document.addEventListener('DOMContentLoaded', {})
+document.addEventListener('DOMContentLoaded', () => {
+  setPlanDatesRange()
+})
 
-const ctx = _('myChart')
+planDateStart.addEventListener('change', (e) => {
+  const value = e.target.value
+  if (value === '' || value.startsWith(0)) return
+  planDateEnd.value = value
+  const datesDifference = getDaysDifference(value)
+  setPlanEndDatesRange(datesDifference, datesDifference + 366)
+  setDaysQuantity()
+})
+
+planDateEnd.addEventListener('change', (e) => {
+  const value = e.target.value
+  if (value === '' || value.startsWith(0)) return
+  setDaysQuantity()
+})
+
+function setDaysQuantity() {
+  let ds = planDateStart.value
+  let de = planDateEnd.value
+  if (ds === '' || de === '' || de.startsWith(0) || ds.startsWith(0)) return
+  const daysBetween = {
+    allDays: 0,
+    workDays: 0,
+  }
+  while (ds !== de) {
+    daysBetween.allDays++
+    if (new Date(ds).getDay() !== 0) daysBetween.workDays++
+    ds = new Date(new Date(ds).getTime() + 24 * 60 * 60 * 1000)
+      .toISOString()
+      .replace(/T.+/g, '')
+  }
+  daysBetween.allDays++
+  if (new Date(ds).getDay() !== 0) daysBetween.workDays++
+  _('planDaysTotal').textContent = daysBetween.allDays.toString()
+  _('planDaysWork').textContent = `${daysBetween.workDays.toString()})`
+}
+
+function getDaysDifference(value) {
+  const diff = parseInt((new Date(value).getTime() - Date.now()) / 86_400_000)
+  return diff > 0 ? 1 + diff : diff
+}
+
+function setPlanDatesRange() {
+  setPlanStartDatesRange()
+  setPlanEndDatesRange()
+}
+
+function setPlanStartDatesRange(
+  daysFromTodayForMin = -31,
+  daysFromTodayForMax = 365
+) {
+  planDateStart.setAttribute(
+    'min',
+    new Date(Date.now() + daysFromTodayForMin * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .replace(/T.+/g, '')
+  )
+  planDateStart.setAttribute(
+    'max',
+    new Date(Date.now() + daysFromTodayForMax * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .replace(/T.+/g, '')
+  )
+}
+
+function setPlanEndDatesRange(
+  daysFromTodayForMin = -30,
+  daysFromTodayForMax = 366
+) {
+  planDateEnd.setAttribute(
+    'min',
+    new Date(Date.now() + daysFromTodayForMin * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .replace(/T.+/g, '')
+  )
+  planDateEnd.setAttribute(
+    'max',
+    new Date(Date.now() + daysFromTodayForMax * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .replace(/T.+/g, '')
+  )
+}
 
 function resetHelpingCalcForm() {
   _('helpingCalcForm').reset()
@@ -14,6 +100,8 @@ function resetHelpingCalcForm() {
 
 function resetGeneralPlanInfoForm() {
   _('generalPlanInfoForm').reset()
+  _('planDaysTotal').textContent = '0'
+  _('planDaysWork').textContent = '0)'
 }
 
 function onChangeCalcDrop1(e) {
@@ -42,7 +130,7 @@ function onChangeCalcDrop1(e) {
 }
 
 function calculateHelpingResult(e) {
-  numberInputPattern(e)
+  floatInputPattern(e)
   const calcInput1 = _('calcInput1').value
   const calcInput2 = _('calcInput2').value
   if (calcInput1 && calcInput2) {
@@ -54,8 +142,12 @@ function calculateHelpingResult(e) {
   }
 }
 
-function numberInputPattern(e) {
+function floatInputPattern(e) {
   e.value = e.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1')
+}
+
+function intInputPattern(e) {
+  e.value = e.value.replace(/\D/g, '')
 }
 
 function setHelpingCategory() {
@@ -68,21 +160,40 @@ function setHelpingCategory() {
   _('calcResultValue').textContent = ''
 }
 
-new Chart(ctx, {
+new Chart(chartJobPlan, {
   type: 'line',
   data: {
     labels: ['5 wrz', '6 wrz', '7 wrz', '8 wrz', '9 wrz', '10 wrz', '11 wrz'],
     datasets: [
       {
-        label: 'Plan',
+        label: 'Plan wykonywania',
         data: [65, 120, 155, 186, 220, 268, 300],
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
       },
+    ],
+  },
+  options: {
+    animations: {
+      tension: {
+        duration: 500,
+        easing: 'easeOutBounce',
+        from: 1,
+        to: 0,
+      },
+    },
+  },
+})
+
+new Chart(chartEmployeesPlan, {
+  type: 'line',
+  data: {
+    labels: ['5 wrz', '6 wrz', '7 wrz', '8 wrz', '9 wrz', '10 wrz', '11 wrz'],
+    datasets: [
       {
         type: 'bar',
-        label: 'Zasoby plan',
+        label: 'Pracowniki plan',
         data: [6, 7, 6, 5, 7, 7, 7],
         backgroundColor: 'rgb(75, 192, 192)',
       },
